5b30bbb6f47be6be00948e62b795f798dd612ded
qingbing <780042175@qq.com>
2021-12-28 15:29:13 +0800

    202112281529

--- wechat/index.php
+++ wechat/index.php
@@ -5,3 +5,30 @@
  * @copyright   Chengdu Qb Technology Co., Ltd.
  */
 
+require("vendor/autoload.php");
+
+\Zf\Wechat\Wechat::checkSignature('qingbing-test'); exit;
+
+
+exit;
+
+
+$options = [
+    'app_id' => 'wxf9661570cc5819d3',
+    'secret' => '0a36801f7dc4b4b7f3d97a74bb927d0a',
+];
+
+
+$app = \EasyWeChat\Factory::officialAccount($options);
+
+$server = $app->server;
+$user   = $app->user;
+
+$server->push(function ($message) use ($user) {
+    $fromUser = $user->get($message['FromUserName']);
+
+    return "{$fromUser->nickname} 您好！欢迎关注 TEST!";
+});
+
+$server->serve()->send();
+
--- wechat/src/Wechat.php
+++ wechat/src/Wechat.php
@@ -7,8 +7,28 @@
 
 namespace Zf\Wechat;
 
-
 class Wechat
 {
+    /**
+     * 验证
+     * @param $token
+     * @return bool
+     */
+    public static function checkSignature($token)
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce     = $_GET["nonce"];
 
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            echo $_GET['echostr'] ?? 'error';
+        } else {
+            return false;
+        }
+    }
 }
