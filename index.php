92c96a0d36c9a9a76bf45656f9ef776ac421341a
qingbing <780042175@qq.com>
2021-12-28 15:03:21 +0800

    202112281503

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
+var_dump(333);
---
+++ wechat/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
